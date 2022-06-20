dc7ea99df5e452a63e509cea5a3cd1db65b941e0
Torukuru Misheal <torukurumisheal2@gmail.com>
2022-06-20 04:26:45 +0100

    algo2

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,5 +1,7 @@
 function filterArray(arr) {
- 
+    const onlyNumbers = arr.filter(
+        element => typeof element === 'number'
+      );
 }
-
+filterArray([1, 2, "a", "b"]);
 module.exports = filterArray
