d03bf671c767753e51cbf615df803610d88c16d4
Torukuru Misheal <torukurumisheal2@gmail.com>
2022-06-20 05:51:19 +0100

    Algo 5

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,11 @@
 function filterList(arr) {
- 
+    var onlyNumbers = arr.filter(
+        element => typeof element === 'number'
+      );
+      console.log (onlyNumbers);
  }
+ filterList([1,2,'a','b']);
+ filterList([1,'a','b',0,15])
+ filterList([1,2,'aasf','1','123',123])
 
   module.exports =filterList
